9f25f7abc396cc9cb957def7d03296adecd779bc
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2021-08-15 13:38:07 +0300

    solved issue with router redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import {Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 import store from '@/store';
-import history from '@/history';
 import GlobalStyles from '@/components/GlobalStyles';
 import Routes from './Routes';
 
 const App = (): JSX.Element => (
   <Provider store={store}>
-    <Router history={history}>
+    <Router>
       <GlobalStyles/>
       <Routes/>
     </Router>
