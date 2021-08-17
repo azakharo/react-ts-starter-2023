d8398313b66b28d89e09ab5c3987a8a1753ed403
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2021-08-17 08:10:55 +0300

    installed eslint plugins and configs, changed eslint config. Not fixed lint errors yet

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,79 @@ module.exports = {
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaVersion": 2018,
-    "sourceType": "module"
+    "sourceType": "module",
+    project: "./tsconfig.json",
   },
-  "plugins": ["@typescript-eslint", "react-hooks"],
   "extends": [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+//    "plugin:jest/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "prettier",
   ],
-  "rules": {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/prop-types": "off"
+  "plugins": [
+    "@typescript-eslint",
+    "eslint-comments",
+    "import",
+    // "jest",
+    "jsx-a11y",
+    "promise",
+    "react",
+    "react-hooks",
+    "unicorn",
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
   },
   "settings": {
     "react": {
       "pragma": "React",
       "version": "detect"
     }
-  }
+  },
+  "rules": {
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    "no-prototype-builtins": "off",
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    "react/destructuring-assignment": "off",
+    "react/prop-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+    "react/jsx-filename-extension": "off",
+    // Use function hoisting to improve code readability
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true },
+    ],
+    // Allow most functions to rely on type inference. If the function is exported, then `@typescript-eslint/explicit-module-boundary-types` will ensure it's typed.
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    // Common abbreviations are known and readable
+    "unicorn/prevent-abbreviations": "off",
+    // Airbnb prefers forEach
+    "unicorn/no-array-for-each": "off",
+    // It's not accurate in the monorepo style
+    "import/no-extraneous-dependencies": "off",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        // Allow `require()`
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
 };
