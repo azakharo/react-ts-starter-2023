96eba838b69e591d86fd29f5b23b31f42e37ae18
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-01-07 12:20:56 +0300

    re-worked the main comp

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,103 +1,112 @@
 import React, {memo} from 'react';
-import {Button} from '@mui/material';
-// import makeStyles from '@mui/styles/makeStyles';
-// import createStyles from '@mui/styles/createStyles';
-// import {CSSProperties} from '@mui/styles';
+import {Box, Button} from '@mui/material';
 
 import errorImg from 'IMAGES/error-page-icon.png';
 import useAuth from 'src/hooks/useAuth';
 
-//const areaColor = '#e2deed';
+const areaColor = '#e2deed';
 
-// const asideMainCommonStyles: CSSProperties = {
-//   minHeight: '300px',
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-//   padding: '1rem',
-//   fontWeight: 'bold',
-// };
-//
-// const useStyles = makeStyles(() =>
-//   createStyles({
-//     container: {
-//       height: '100vh',
-//       display: 'grid',
-//       gridTemplateAreas: `"header header"
-//       "sidebar main"
-//       "footer footer"`,
-//       gridTemplateColumns: 'minmax(200px, 20%) 1fr',
-//       gridTemplateRows: '3rem 1fr 3rem',
-//       gap: '1rem',
-//
-//       '&> *': {
-//         backgroundColor: areaColor,
-//       },
-//     },
-//
-//     header: {
-//       gridArea: 'header',
-//       display: 'flex',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//       padding: '0 16px',
-//     },
-//
-//     logo: {
-//       height: '2rem',
-//       marginRight: 'auto',
-//     },
-//
-//     greeting: {
-//       marginRight: '16px',
-//     },
-//
-//     username: {
-//       fontWeight: 'bold',
-//     },
-//
-//     main: {
-//       gridColumn: 'main',
-//       ...asideMainCommonStyles,
-//     },
-//
-//     aside: {
-//       gridArea: 'sidebar',
-//       ...asideMainCommonStyles,
-//     },
-//
-//     footer: {
-//       gridArea: 'footer',
-//       fontWeight: 'bold',
-//       display: 'flex',
-//       alignItems: 'center',
-//       padding: '0 16px',
-//     },
-//   }),
-// );
+const asideMainCommonStyles = {
+  minHeight: '300px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '1rem',
+  fontWeight: 'bold',
+};
 
 const Main = () => {
   const {logout, user} = useAuth();
   const username = user ? user.name : '';
 
   return (
-    <div className={'classes.container'}>
-      <header className={'classes.header'}>
-        <img className={'classes.logo'} src={errorImg as string} alt="logo" />
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'grid',
+        gridTemplateAreas: `"header header"
+      "sidebar main"
+      "footer footer"`,
+        gridTemplateColumns: 'minmax(200px, 20%) 1fr',
+        gridTemplateRows: '3rem 1fr 3rem',
+        gap: '1rem',
+
+        '&> *': {
+          backgroundColor: areaColor,
+        },
+      }}
+    >
+      <Box
+        component="header"
+        sx={{
+          gridArea: 'header',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '0 16px',
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            height: '2rem',
+            marginRight: 'auto',
+          }}
+          src={errorImg as string}
+          alt="logo"
+        />
         {username && (
-          <span className={'classes.greeting'}>
+          <Box
+            sx={{
+              marginRight: '16px',
+            }}
+          >
             <span>Hello </span>
-            <span className={'classes.username'}>{username}</span>
-          </span>
+            <Box
+              component="span"
+              sx={{
+                fontWeight: 'bold',
+              }}
+            >
+              {username}
+            </Box>
+          </Box>
         )}
         <Button variant="contained" color="primary" onClick={logout}>
           Logout
         </Button>
-      </header>
-      <aside className={'classes.aside'}>Sidebar</aside>
-      <main className={'classes.main'}>Main</main>
-      <footer className={'classes.footer'}>Footer</footer>
-    </div>
+      </Box>
+      <Box
+        component="aside"
+        sx={{
+          gridArea: 'sidebar',
+          ...asideMainCommonStyles,
+        }}
+      >
+        Sidebar
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          gridColumn: 'main',
+          ...asideMainCommonStyles,
+        }}
+      >
+        Main
+      </Box>
+      <Box
+        component="footer"
+        sx={{
+          gridArea: 'footer',
+          fontWeight: 'bold',
+          display: 'flex',
+          alignItems: 'center',
+          padding: '0 16px',
+        }}
+      >
+        Footer
+      </Box>
+    </Box>
   );
 };
 
