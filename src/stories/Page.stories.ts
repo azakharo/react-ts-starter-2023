fff3576576b972ded27f145afed40272603fb30b
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-08-23 11:53:00 +0300

    auto fix some minor issues

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,7 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+import type {Meta, StoryObj} from '@storybook/react';
+import {userEvent, within} from '@storybook/testing-library';
 
-import { Page } from './Page';
+import {Page} from './Page';
 
 const meta = {
   title: 'Example/Page',
@@ -19,10 +19,10 @@ export const LoggedOut: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
+  play: async ({canvasElement}) => {
     const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
+    const loginButton = canvas.getByRole('button', {
+      name: /log in/i,
     });
     await userEvent.click(loginButton);
   },
